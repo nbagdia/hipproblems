43be46331a1a61bf5c8a8feba629170ed157ee32
Neha Bagdia <nbagdia@gmail.com>
2018-02-25 15:26:17 -0800

    Updating the self.data, removing decoders, adding comments.

--- searchrunner/index.py
+++ searchrunner/index.py
@@ -4,55 +4,70 @@ import urllib2
 import threading
 from urllib2 import URLError
 
+#map to localhost scraper url
 scraperurl = "http://localhost:9000/scrapers/"
+#provider list
 scraperProvider = ["expedia", "orbitz", "priceline", "travelocity", "united "]
+#port no to publish merged list
 portno = 8000
 
 class ApiHandler(web.RequestHandler):
-    data = []
     @gen.coroutine
     def get(self):
-        self.write({ 
-            "results": self.getResults()
-        })
+        try :
+            self.write({ 
+                "results": self.getResults()
+                })
+        except :
+            self.write({
+                "Error during thread execution"
+                })
         
     def fetch_url(self, url):
         try:
             r = urllib2.urlopen(url)
-            #JSON encoding breaks into Unicode by default, hence force read into charset or UTF-8
-            self.data.extend(json.loads(r.read().decode(r.info().get_param('charset') or 'utf-8'))["results"])
+            self.data.extend(json.loads(r.read())["results"])
         except URLError:
+            #print the url which returned error
             self.write({
                 "Error fetching URL" : url
                 })
-            return
         except ValueError:
+            #print if Json data issues
             self.write({
                 "Error in Json Data from URL" : url
                 })
-            return
+            
     
-    def getResults(self):    
+    def getResults(self):  
+        #reset self.data to prevent duplication of results over different calls
+        self.data = []  
+        # threads to process different providers simultaneously
         threads = [threading.Thread(target=self.fetch_url, args=(scraperurl+provider,)) for provider in scraperProvider]
         for thread in threads:
             thread.start()
         for thread in threads:
             thread.join()
+        #Sort the data
         newlist = sorted(self.data, key=lambda k: k['agony'])
         return (newlist)
-        
+      
 ROUTES = [
     (r"/flights/search", ApiHandler),
 ]
 
 def run():
-    app = web.Application(
-        ROUTES,
-        debug=True,
-    )
-    app.listen(portno)
-    print("Server (re)started. Listening on port %s" %portno)
-    ioloop.IOLoop.current().start()
+    try :
+        app = web.Application(
+            ROUTES,
+            debug=True,
+        )
+        app.listen(portno)
+        print("Server (re)started. Listening on port %s" %portno)
+        ioloop.IOLoop.current().start()
+    except :
+        print("Cannot start server on port %s" %portno)
+        print("Check if it already running on a different terminal")
 
 if __name__ == "__main__":
     run()
